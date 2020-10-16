3e981782ef662cbfd9a262e67566e549b0d599c2
Jason White <jmw5598@gmail.com>
2020-10-16 15:47:43 -0400

    Create seed data and migration for seed data

--- server/src/database/migrations/1602867906847-InitialMigrationForUsersEntity.ts
+++ server/src/database/migrations/1602867906847-InitialMigrationForUsersEntity.ts
@@ -1,14 +1,13 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class InitialMigrationForUsersEntity1602867906847 implements MigrationInterface {
-    name = 'InitialMigrationForUsersEntity1602867906847'
+  name = 'InitialMigrationForUsersEntity1602867906847'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "firstName" varchar NOT NULL, "lastName" varchar NOT NULL, "email" varchar NOT NULL)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "firstName" varchar NOT NULL, "lastName" varchar NOT NULL, "email" varchar NOT NULL)`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
