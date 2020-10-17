4061b5867960efda0c57b85353f818e6a32fcd7f
Jason White <jmw5598@gmail.com>
2020-10-17 18:26:10 -0400

    Simplified pagination models

--- client/src/app/core/models/page-request.model.ts
+++ client/src/app/core/models/page-request.model.ts
@@ -11,18 +11,6 @@ export class PageRequest {
     this.sort = sort;
   }
 
-  public next(totalElements:number): PageRequest {
-    const totalPages: number = Math.ceil(totalElements / this.size) || 1;
-    const nextPage: number = +this.page === totalPages ? 1 : +this.page + 1;
-    return new PageRequest(nextPage, this.size, this.sort);
-  }
-
-  public previous(totalElements: number): PageRequest {
-    const totalPages: number = Math.ceil(totalElements / this.size || 1);
-    const previousPage: number = +this.page=== 1 ? totalPages : +this.page- 1;
-    return new PageRequest(previousPage, this.size, this.sort);
-  }
-
   public static from(page: number, size: number, sortColumn: string, sortDirection: string): PageRequest {
     const sort: Sort = Sort.from(sortColumn, sortDirection);
     return new PageRequest(page, size, sort);
--- client/src/app/core/models/sort.model.ts
+++ client/src/app/core/models/sort.model.ts
@@ -9,20 +9,6 @@ export class Sort {
     this.column = column;
   }
 
-  public getSortDirection(): SortDirection {
-    return this.direction;
-  }
-
-  public getSortColumn(): string {
-    return this.column;
-  }
-
-  public asKeyValue(): { [key: string]: string } {
-    return {
-      [this.getSortColumn()]: this.getSortDirection()
-    };
-  }
-
   public static from(column: string, direction: string): Sort {
     switch (direction.toUpperCase()) {
       case 'ASC': return new Sort(column, SortDirection.ASCENDING);
