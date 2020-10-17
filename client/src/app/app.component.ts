880cffa334f21c2e2961fb822adbf763ed825fa0
Jason White <jmw5598@gmail.com>
2020-10-17 15:31:26 -0400

    fixed merge conflict

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './core/services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
-
-  public nextPage(): void {
-
-  }
-
-  public prevPage(): void {
-    
+export class AppComponent implements OnInit {
+  constructor(
+    private _usersService: UsersService
+  ) {}
+  
+  ngOnInit(): void {
+    this._usersService.findAll()
+      .subscribe(page => console.log(page));
   }
 }
